3f4755a632b202331f05f4dcdcfdc1ebf4f5387a
Anıl Şenay <anilsenay3@gmail.com>
2018-12-15 02:27:53 +0300

    time zone issue fixed, it takes local time of user now

--- timepassword.py
+++ timepassword.py
@@ -1,7 +1,7 @@
-from time import gmtime, strftime
+from datetime import datetime
 
 username = "admin"
-password = str(int(strftime("%d%m%H%M", gmtime()))+300) ## +300 for GMT+3 which is my timezone
+password = str(int(datetime.now().strftime("%d%m%H%M")))
 
 signed = False
 attempts = 3;
@@ -9,7 +9,7 @@ while(signed == False):
 
 	username = input("User name: ")
 	passwd = input("Password: ")
-
+	print(password)
 	if(username == "admin" and passwd == password):
 		print("You signed in")
 		signed = True
